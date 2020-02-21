8b5367c05eebcab814fa7cab0db99b01f554c00a
Maicon Melo Alves <maiconmelo.ufrj@gmail.com>
2020-02-21 08:32:42 -0300

    .

--- code/cnpj/criar_favorecidos.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE favorecidos (
-  cnpj varchar(14) PRIMARY KEY,
-  atividadePrincipal varchar(300),
-  uf varchar(2),
-  municipio varchar(100)
-);
-
-select * from favorecidos;
--- code/cnpj/extrair_dados_favorecidos.sh
+++
@@ -1,1 +0,0 @@
-pg_dump -h localhost -p 5432 -U postgres -W --table="favorecidos" --data-only --column-inserts sa > dados_favorecidos.sql
